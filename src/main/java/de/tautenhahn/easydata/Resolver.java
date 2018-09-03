f6bf9525e331956d88cd59cd97e029e61b27f14f
Dr. Thomas Tautenhahn <t-tautenhahn@gmx.de>
2018-09-03 23:21:03 +0200

    some ideas

---
+++ src/main/java/de/tautenhahn/easydata/Resolver.java
@@ -0,0 +1,24 @@
+package de.tautenhahn.easydata;
+
+import java.io.Writer;
+
+
+/**
+ * Just the ability to resolve some part of the template using the given data.
+ * 
+ * @author TT
+ */
+public interface Resolver
+{
+
+  /**
+   * Writes result of transformation into given Writer.
+   * 
+   * @param startTag just passed in to allow singleton instances when there is not much to do.
+   * @param data all data available within the current context, namely original data plus some attributes
+   *          defined in surrounding tags.
+   * @param output
+   */
+  void resolve(Token start, Object data, Writer output);
+
+}
---
+++ src/main/java/de/tautenhahn/easydata/Token.java
@@ -0,0 +1,54 @@
+package de.tautenhahn.easydata;
+
+/**
+ * Wraps some part of the template file together with its position in the original template.
+ * 
+ * @author TT
+ */
+public class Token
+{
+
+  private final String content;
+
+  private final int row;
+
+  private final int col;
+
+  /**
+   * Creates immutable instance
+   * 
+   * @param content
+   * @param row
+   * @param col
+   */
+  public Token(String content, int row, int col)
+  {
+    this.content = content;
+    this.row = row;
+    this.col = col;
+  }
+
+  /**
+   * Returns represented template part.
+   */
+  public String getContent()
+  {
+    return content;
+  }
+
+  /**
+   * Returns original row.
+   */
+  public int getRow()
+  {
+    return row;
+  }
+
+  /**
+   * Returns original column.
+   */
+  public int getCol()
+  {
+    return col;
+  }
+}
